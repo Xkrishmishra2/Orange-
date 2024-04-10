78a96121428b44302a2a385d20a2c7e951c08f6c
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-10 23:07:13 +0530

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -11,14 +11,14 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="DAXXAss1",
+            name="Xkrishmishra",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="DAXXAss2",
+            name="Xkrishmishra",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("ALLTYPECC")
-                await self.one.join_chat("DAXXSUPPORT")
+                await self.five.join_chat("speeds_net")
+                await self.one.join_chat("speeds_net")
             except:
                 pass
             assistants.append(5)
