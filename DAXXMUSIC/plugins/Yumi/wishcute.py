00da40602763478f333dee59ae09722c211f6fd7
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-11 01:46:52 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
-from DAXXMUSIC import app 
+from SPEEDMUSIC import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "speeds_net"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
