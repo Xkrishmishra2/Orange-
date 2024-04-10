996da4992d7353ea0d7b3053ead8cecb675c7d24
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-11 02:28:00 +0530

    Update mustjoin.py

--- DAXXMUSIC/plugins/bot/mustjoin.py
+++ DAXXMUSIC/plugins/bot/mustjoin.py
@@ -2,11 +2,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from DAXXMUSIC import app
+from SPEEDMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "ALLTYPECC"
+MUST_JOIN = "speeds_net"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
