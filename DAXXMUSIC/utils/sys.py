90fff5ad2c437a42ff028030f66758fa09310d45
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-10 21:58:02 +0530

    Update sys.py

--- DAXXMUSIC/utils/sys.py
+++ DAXXMUSIC/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from DAXXMUSIC.misc import _boot_
-from DAXXMUSIC.utils.formatters import get_readable_time
+from SPEEDMUSIC.misc import _boot_
+from SPEEDMUSICl.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
