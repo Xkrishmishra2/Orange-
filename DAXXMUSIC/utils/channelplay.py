cbfbdf39b8db6a6d5af7441ddec07466e596d7fd
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-10 21:54:39 +0530

    Update channelplay.py

--- DAXXMUSIC/utils/channelplay.py
+++ DAXXMUSIC/utils/channelplay.py
@@ -1,5 +1,5 @@
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_cmode
+from SPEEDMUSIC import app
+from SPEEDMUSIC.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
