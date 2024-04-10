c27673380c08bbe8d9c63bedae5068e12dbce215
Xkrishmishra <163736172+Xkrishmishra@users.noreply.github.com>
2024-04-10 18:59:07 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -1,8 +1,8 @@
-from DAXXMUSIC.core.bot import DAXX
-from DAXXMUSIC.core.dir import dirr
-from DAXXMUSIC.core.git import git
-from DAXXMUSIC.core.userbot import Userbot
-from DAXXMUSIC.misc import dbb, heroku
+from SPEEDNET.core.bot import SPEED
+from SPEEDNET.core.dir import dirr
+from SPEEDNET.core.git import git
+from SPEEDNET.core.userbot import Userbot
+from SPEEDNET.misc import dbb, heroku
 
 from SafoneAPI import SafoneAPI
 from .logging import LOGGER
